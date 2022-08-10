b1c9811bddaa7046e14c2711cb71e322e06d402e
Eric Wen <wen.ebc20@gmail.com>
2022-08-11 03:25:49 +0800

    Addition of stock list page on Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from './header/Header'
 import Bankers from "./dashboard/Bankers"
+
 import animations from './invest/questionnaire.js'
 
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ProductsList } from './dashboard/ProductsList';
+import { ProductDetails } from './dashboard/ProductDetails';
+
+const Stack = createNativeStackNavigator();
 class App extends Component {
+
   render() {
 
     const headerItems = [
@@ -37,9 +46,30 @@ class App extends Component {
           <Route exact path="dashboard" element={<Bankers />} />
           <Route exact path="invest" element={<animations />} />
         </Routes>
+
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name='Products' component={ProductsList}
+                          options={() => ({
+                            title: 'Products',
+                            headerTitleStyle: styles.headerTitle
+                          })}/>
+            <Stack.Screen name='ProductDetails' component={ProductDetails}
+                          options={() => ({
+                            title: 'Product details',
+                            headerTitleStyle: styles.headerTitle
+                          })} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  headerTitle: {
+    fontSize: 20
+  }
+});
 
 export default App;

46b65ffc53a842a9e70f31949392b1f6d831ffce
fainttttt <87219681+fainttttt@users.noreply.github.com>
2022-08-11 18:33:47 +0800

    Create data.js

---
+++ client/src/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        text: 'Application Opening',
+        date: '3 May 2022',
+        category: {
+            tag: 'Amazing!',
+            color: '#003B7D'
+        }
+    },
+    {
+        text: 'First Deposit with Citi',
+        date: '8 May 2022',
+        category: {
+            tag: "Splendid!",
+            color: '#003B7D'
+        }
+    },
+    {
+        text: '100 days with Citi',
+        date: '11 Aug 2022',
+        category: {
+            tag: 'Time flies!',
+            color: '#003B7D'
+        }
+    }
+]
+
+export default data;

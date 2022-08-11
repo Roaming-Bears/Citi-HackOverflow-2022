6fffb6e8554442ca5d739717590354f3dc26be96
Eric Wen <wen.ebc20@gmail.com>
2022-08-12 06:05:19 +0800

    commit some additional files to client and banker milestone

---
+++ client/src/dashboard/client-milestone/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        text: 'Created Account',
+        date: '3 May 2022',
+        category: {
+            tag: 'Account',
+            color: '#003B7D'
+        }
+    },
+    {
+        text: 'Purchased 1st investment plan',
+        date: '6 May 2022',
+        category: {
+            tag: "Investment",
+            color: '#003B7D'
+        }
+    },
+    {
+        text: 'Saved $100,000',
+        date: '9 May 2022',
+        category: {
+            tag: 'Savings',
+            color: '#003B7D'
+        }
+    }
+]
+
+export default data;
---
+++ client/src/homepage/NewsContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useEffect, useState } from 'react';
+import axios from "axios";
+
+export const NewsContext = createContext()
+
+export const NewsContextProvider = (props) => {
+    const [data, setData] = useState()
+    const apiKey = "567845fae7a74ce3b4e51bdec331b995";
+
+    useEffect(() => {
+        axios.get(
+            `https://newsapi.org/v2/top-headlines?domains=wsj.com&apiKey=${apiKey}`
+        ).then((response) => setData(response.data))
+            .catch((error) => console.log(error));
+
+
+    }, []);
+
+    return(
+        <NewsContext.Provider value={{ data }}>
+            {props.children}
+        </NewsContext.Provider>
+    )
+}
+
